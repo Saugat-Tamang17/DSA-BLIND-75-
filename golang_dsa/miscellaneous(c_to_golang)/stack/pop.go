4a04e438aba7481da347c12aa90972597a27856a
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-13 19:54:22 +0545

    pop function for the stack data structure, i guess,please work

---
+++ golang_dsa/miscellaneous(c_to_golang)/stack/pop.go
@@ -0,0 +1,15 @@
+package stack
+
+import "fmt"
+
+func (s *Stack) Pop() int {
+	if is.IsEmpty() {
+		fmt.Println("Stack Underflow sarkaar ! Nothing to pop.")
+		return -1
+	}
+	value := s.data[s.top]
+	s.top--
+	fmt.Println("Popped:", value)
+	return value
+
+}
