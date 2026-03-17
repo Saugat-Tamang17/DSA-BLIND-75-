555b443fb7029e1c866c7087fe8544a517c66771
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-03-17 19:06:21 +0545

    selection method , nothing else for now

---
+++ golang_dsa/sorting/Comparison/methods/selection.go
@@ -0,0 +1,19 @@
+package methods
+
+func SelectionSort(arr []int) {
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+
+		// Find the index of the minimum element
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+
+		// Swap the found minimum element with the first element
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+}
